b526f2d693b540c8248b987b6a9de4fcdd05814a
DemetriousWilson <DemetriousWilson@gmail.com>
2013-09-12 17:06:09 -0700

    Add Constructor code to Patomic and PatomicShell

--- src/Patomic.php
+++ src/Patomic.php
@@ -12,13 +12,18 @@ class Patomic
                 "dir"           => "",
                 "port"          => 0,
                 "storage"       => "",
+                "alias"         => "",
         );
         private $pShell = null;
 
-        public function __construct($dir = null, $port = 9998, $storage = "mem", PatomicShell $pShell = null ) {
+        public function __construct($dir = null, $port = 9998, $storage = "mem", $alias = null, PatomicShell $pShell = null ) {
                 try {
                         if(!isset($dir)) {
                                 throw new PatomicException("Could find datomic folder");
+                        }
+
+                        if(!isset($alias)) {
+                                throw new PatomicException("Datomic alias not set");
                         }
 
                         if(!isset($pShell)) {
@@ -28,8 +33,10 @@ class Patomic
                         $this->datomic["dir"]           = $dir;
                         $this->datomic["port"]          = $port;
                         $this->datomic["storage"]       = $storage;
+                        $this->datomic["alias"]         = $alias;
 
                         $this->pShell = $pShell;
+                        $this->pShell->setConfig($this->datomic);
                 } catch(PatomicException $e) {
                         echo $e;
                 }
--- src/PatomicShell.php
+++ src/PatomicShell.php
@@ -6,4 +6,16 @@
  */
 class PatomicShell
 {
+        private $config = array();
+
+        public function startService() {
+                $script = "cd ".$this->config["dir"].";"; 
+                $script .= "./bin/rest -p ".$this->config["port"];
+                //$script .=; 
+                shell_exec($script);
+        }
+
+        public function setConfig(Array $datomic) {
+                $this->config = $datomic;
+        }
 }
