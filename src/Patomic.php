a61132c8e0c895f7272b822780df51ab8087d84e
DemetriousWilson <DemetriousWilson@gmail.com>
2013-09-13 00:12:10 -0700

    Work in progress just trying some things out

--- src/Patomic.php
+++ src/Patomic.php
@@ -5,21 +5,27 @@ require "PatomicShell.php";
 /**
  * Main class for Patomic
  * Every instance of this class represents a connection to the Datomic REST service
+ * Uses Observer pattern to notify PatomicShell
+ *
+ * @see http://www.php.net/manual/en/class.splobserver.php
  */
-class Patomic
+class Patomic implements SplSubject
 {
-        private $datomic = array(
+        private $shellConfig = array(
                 "dir"           => "",
                 "port"          => 0,
                 "storage"       => "",
                 "alias"         => "",
         );
-        private $pShell = null;
+        private $storageTypes   = array("mem", "dev", "sql", "inf", "ddb");
+        private $isRunning      = false;
+        private $observers      = array();
+        private $status         = "";
 
-        public function __construct($dir = null, $port = 9998, $storage = "mem", $alias = null, PatomicShell $pShell = null ) {
+        public function __construct($dir = null, $port = 9998, $storage = "mem", $alias = null, PatomicShell $pShell = null) {
                 try {
                         if(!isset($dir)) {
-                                throw new PatomicException("Could find datomic folder");
+                                throw new PatomicException("Did not provide a value for the Datomic directory");
                         }
 
                         if(!isset($alias)) {
@@ -30,15 +36,55 @@ class Patomic
                                 throw new PatomicException("Must add a PatomicShell object to this constructor");
                         }
 
-                        $this->datomic["dir"]           = $dir;
-                        $this->datomic["port"]          = $port;
-                        $this->datomic["storage"]       = $storage;
-                        $this->datomic["alias"]         = $alias;
+                        $this->shellConfig["dir"]           = $dir;
+                        $this->shellConfig["port"]          = $port;
+                        $this->shellConfig["storage"]       = $storage;
+                        $this->shellConfig["alias"]         = $alias;
 
-                        $this->pShell = $pShell;
-                        $this->pShell->setConfig($this->datomic);
+                        $pShell->setConfig($this->shellConfig);
+
+                        $pShell->startService();
+
+                        $this->attach($pShell);
+
+                        $this->isRunning = true;
+                        $this->status = "datomic:$storage:// $alias running on localhost:$port"; 
+                        $this->notify();
                 } catch(PatomicException $e) {
                         echo $e;
                 }
         }
+
+        public function getStatus() {
+                return $this->status;
+        }
+
+        /**
+         * @override
+         */
+        public function attach(SplObserver $obs) {
+                $this->observers[] = $obs;        
+        }
+
+        /**
+         * @override
+         */
+        public function detach(SplObserver $obs) {
+                $key = array_search($obs, $this->observers, true);
+                if($key) {
+                        unset($this->observers[$key]);
+                }
+        }
+
+        /**
+         * @override
+         */
+        public function notify() {
+                foreach($this->observers as $observer) {
+                        $observer->update($this);
+                }
+        }
 }
+
+$ps = new PatomicShell();
+$patomic = new Patomic("/Users/demetriouswilson/Documents/datomic", 9998, "mem", "patomic", $ps);
