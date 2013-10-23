70391e5d7cf2bf2dcf137b766ec26b1b01975a8f
DemetriousWilson <DemetriousWilson@gmail.com>
2013-10-23 00:42:20 -0700

    Refactor PatomicSchema based on Datomic attribute and schema research

--- src/PatomicSchema.php
+++ src/PatomicSchema.php
@@ -9,6 +9,8 @@ require_once "PatomicException.php";
 class PatomicSchema
 {
     private $name;
+    private $namespace;
+    private $identity;
     private $valueType;
     private $schema;
     private $schemaDef = array(
@@ -35,41 +37,70 @@ class PatomicSchema
             "index",
             "fulltext",
             "isComponent",
-            "noHistory",
+            "noHistory"
         )
     );
 
-    public function __construct($name, $valueType) {
-        if(!in_array($valueType, $this->schemaDef['db']['valueType'])) {
-            throw new PatomicException("Invalid schema valueType, the valueType must be "
-            . print_r($this->schemaDef["db"]["valueType"], true));
-        }
-        if(!is_string($name)) {
-            throw new PatomicException(__METHOD__ . " expects \$name to be a string");
-        }
+    /**
+     * Creates the :db/id, :db/ident and :db/valueType for a new Datomic attribute
+     * as a part of a schema
+     *
+     * @param string $name
+     * @param string $namespace
+     * @param string $identity
+     * @param string $valuetype
+     *
+     * @return A new Datomic attribute with the id, ident and valueType datoms defined
+     */
+    public function __construct($name, $namespace, $identity, $valueType) {
         $this->name = $name;
+        $this->namespace = $namespace;
+        $this->identity = $identity;
         $this->valueType = $valueType;
-        $this->schema = new \igorw\edn\Map();
+        $this->schema = $this->_vector();
+
+        $attr = $this->_map();
+        //$attr[$this->_keyword("db/id")] = $this->_symbol("db/id"
     }
 
     /**
-     * Appends a new entry into the current schema
+     * Appends a new datom into the current attribute
      * <br />
-     * Require at minimum two arguments; the :db symbol and the :db valuetypes
+     * Require at minimum two arguments; the attribute and the value(s)
      * @throws PatomicException
      */
-    public function field() {
+    public function datom() {
         $argc = func_num_args();
         if($argc < 2) {
             throw new PatomicException(__METHOD__ . " requires at least two arguments");
         }
         $argv = func_get_args();
-        $symbol = $argv[0];
+        $attribute = $argv[0];
+        $value = $argv[1];
+        //$this->schema[\igorw\edn\keyword($attribute)
+    }
+
+    // In case I ever need to swap out the EDN datastructures
+    private function _keyword($k) {
+            return \igorw\edn\keyword($k);
+    }
+    private function _symbol($s) {
+            return \igorw\edn\symbol($s);
+    }
+    private function _map() {
+            return new \igorw\edn\Map();
+    }
+    private function _vector() {
+            return new \igorw\edn\Vector();
     }
 }
+
+//Just testing... this will be deleted soon
 $map = new \igorw\edn\Map();
-$map[\igorw\edn\keyword('fName')] = 'Demetrious';
-$map[\igorw\edn\keyword('lName')] = 'Wilson';
+$vec = new \igorw\edn\Vector(array(\igorw\edn\keyword("db.part/db")));
+$tag = new \igorw\edn\Tag("db/id");
+$tagged = new \igorw\edn\Tagged($tag, $vec);
+$map[\igorw\edn\keyword('db/id')] = $tagged;
+$map[\igorw\edn\keyword('db/doc')] = 'Too busy testing';
 $map[\igorw\edn\keyword('db/cardinality')] = \igorw\edn\keyword('db.cardinality/one');
-print_r($map);
 echo \igorw\edn\encode($map).PHP_EOL;
