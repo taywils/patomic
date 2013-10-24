708b4802a8612061e5765f73425a7c2038f4a3ef
DemetriousWilson <DemetriousWilson@gmail.com>
2013-10-23 23:53:33 -0700

    Add edn wrapper methods to PatomicSchema Finish PatomicSchema constructor

--- src/PatomicSchema.php
+++ src/PatomicSchema.php
@@ -46,22 +46,41 @@ class PatomicSchema
      * as a part of a schema
      *
      * @param string $name
+     * @param string identity
      * @param string $namespace
-     * @param string $identity
      * @param string $valuetype
      *
-     * @return A new Datomic attribute with the id, ident and valueType datoms defined
+     * @return PatomicSchema A new Datomic attribute with the id, ident and valueType datoms defined
      */
     public function __construct($name, $namespace, $identity, $valueType) {
         $this->name = $name;
         $this->namespace = $namespace;
         $this->identity = $identity;
         $this->valueType = $valueType;
-        $this->schema = $this->_vector();
 
-        $attr = $this->_map();
-        //$attr[$this->_keyword("db/id")] = $this->_symbol("db/id"
+        $this->schema = $this->_map();
+
+        $idTag = $this->_tag("db/id");
+        $dbPart = $this->_vector(array($this->_keyword("db.part/db")));
+        $idTagged = $this->_tagged($idTag, $dbPart);
+        echo $this->_encode($dbPart).PHP_EOL;
+        $this->schema[$this->_keyword("db/id")] = $idTagged;
+
+        $ident = (is_null($namespace) || '' == $namespace) ? $name : $namespace . "." . $name;
+        $ident .= "/" . $identity;
+        $this->schema[$this->_keyword("db/ident")] = $this->_keyword($ident);
+
+        $this->schema[$this->_keyword("db/valueType")] = $this->_keyword("db.type/" . $valueType);
     }
+
+    /**
+     * @override
+     * @return String
+     */
+    public function __toString() {
+        return $this->_encode($this->schema);
+    }
+
 
     /**
      * Appends a new datom into the current attribute
@@ -77,30 +96,37 @@ class PatomicSchema
         $argv = func_get_args();
         $attribute = $argv[0];
         $value = $argv[1];
-        //$this->schema[\igorw\edn\keyword($attribute)
+        $this->schema[$this->_keyword($attribute)] = $this->_keyword($value);
     }
 
-    // In case I ever need to swap out the EDN datastructures
     private function _keyword($k) {
             return \igorw\edn\keyword($k);
     }
     private function _symbol($s) {
             return \igorw\edn\symbol($s);
     }
-    private function _map() {
-            return new \igorw\edn\Map();
+    private function _map($a = null) {
+            $map = new \igorw\edn\Map();
+
+            if(isset($a) && is_array($a)) {
+                    $map->init($a);
+            }
+
+            return $map;
     }
-    private function _vector() {
-            return new \igorw\edn\Vector();
+    private function _vector($a) {
+            return new \igorw\edn\Vector($a);
+    }
+    private function _tag($t) {
+            return new \igorw\edn\tag($t);
+    }
+    private function _tagged($t, $v) {
+            return new \igorw\edn\Tagged($t, $v);
+    }
+    private function _encode($edn) {
+            return \igorw\edn\encode($edn);
     }
 }
 
-//Just testing... this will be deleted soon
-$map = new \igorw\edn\Map();
-$vec = new \igorw\edn\Vector(array(\igorw\edn\keyword("db.part/db")));
-$tag = new \igorw\edn\Tag("db/id");
-$tagged = new \igorw\edn\Tagged($tag, $vec);
-$map[\igorw\edn\keyword('db/id')] = $tagged;
-$map[\igorw\edn\keyword('db/doc')] = 'Too busy testing';
-$map[\igorw\edn\keyword('db/cardinality')] = \igorw\edn\keyword('db.cardinality/one');
-echo \igorw\edn\encode($map).PHP_EOL;
+$test = new PatomicSchema("league", "ffl", "name", "string");
+echo $test.PHP_EOL;
